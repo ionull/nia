aa6ae30b3f5709735adf92db780b6049a49832c0
Tsung Wu <tsung.bz@gmail.com>
2025-11-11 11:53:49 +0800

    Update version and match URLs in review.user.js
    Updated version and match URLs for the user script.

--- review.user.js
+++ review.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Ingress 一键审PO
 // @namespace    http://tampermonkey.net/
-// @version      2024-10-06
+// @version      2025-11-11
 // @description  在页面最顶端增加了一个按钮 一键勾选所有对话框 减少重复操作
 // @author       PT & ionull
 // @license      MIT
 // @match        https://wayfarer.nianticlabs.com/new/review
+// @match        https://opr.ingress.com/new/review
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nianticlabs.com
 // @grant        none
 // @downloadURL https://github.com/ionull/nia/raw/refs/heads/main/review.user.js
