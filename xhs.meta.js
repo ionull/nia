3013fd70c75de121fd5ca572bc22cb1b94ca386f
Tsung Wu <tsung.bz@gmail.com>
2026-06-12 11:47:15 +0800

    Update version number in xhs.meta.js

--- xhs.meta.js
+++ xhs.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         小红书直播间精简 - 隐藏顶部叠加层与返回按钮
 // @namespace    http://tampermonkey.net/
-// @version      2026-06-12
+// @version      2026-06-12-1
 // @description  隐藏小红书直播间的顶部主播信息叠加层和返回图标
 // @author       Tsung Wu
 // @match        *://www.xiaohongshu.com/livestream/*
