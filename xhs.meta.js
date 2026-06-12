5a0f24071354295ee430e5dba0c9c0b4509ccfdc
Tsung Wu <tsung.bz@gmail.com>
2026-06-12 11:09:20 +0800

    Create xhs.meta.js

---
+++ xhs.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         小红书直播间精简 - 隐藏顶部叠加层与返回按钮
+// @namespace    http://tampermonkey.net/
+// @version      2026-06-12
+// @description  隐藏小红书直播间的顶部主播信息叠加层和返回图标
+// @author       Tsung Wu
+// @match        *://www.xiaohongshu.com/livestream/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
